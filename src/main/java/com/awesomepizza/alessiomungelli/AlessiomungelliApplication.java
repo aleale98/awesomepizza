eec303d7592dadc1170b870703fad632e071cee7
alessio <alessiomungelli@gmail.com>
2026-05-30 17:02:12 +0200

    first commit

---
+++ src/main/java/com/awesomepizza/alessiomungelli/AlessiomungelliApplication.java
@@ -0,0 +1,13 @@
+package com.awesomepizza.alessiomungelli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlessiomungelliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlessiomungelliApplication.class, args);
+	}
+
+}
